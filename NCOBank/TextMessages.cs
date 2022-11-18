23fd0c7e99867d2c854980c1107200fe0767f973
ahlun <ahlun@DESKTOP-0BR3SCO>
2022-11-18 14:21:19 +0100

    added test class for text messages

--- NCOBank/TextMessages.cs
+++ NCOBank/TextMessages.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace NCOBank
 {
-    internal class TextMessages
+    public class TextMessages
     {
+        public static void PressEnter()
+        {
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+        }
+        public static void MessegeColor(string output, bool ok = true)
+        {
+            if (ok)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.WriteLine(output);
+            Console.ResetColor();
+        }
+        public static void YellowMessegeColor(string output)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(output);
+            Console.ResetColor();
+        }
     }
 }
